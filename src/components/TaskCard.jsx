25151ef3b4b36db1e3054eee562fa5cba7c570d6
nnay29 <fodoptallayann@gmail.com>
2025-12-11 11:58:35 +0100

    Add(TaskCard container): Multiple days of the week inside flex container

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,100 +2,102 @@
 import './TaskCard.css'
 import '../index.css'
 
-function TaskCard(){
-    
-    return(
-      <>
-      
-      <div className="task-card-container">
-            <h2>09 Dec <span className="day-of-week">Tue</span></h2>
-           
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-          
-      </div>
+function TaskCard() {
 
-      <div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
+    return (
+        <>
+            <div className="task-card-container">
 
-      </div><div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
+                <div className="task-card">
+                    <h2>09 Dec <span className="day-of-week">Tue</span></h2>
 
-      </div><div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
 
-      </div><div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
 
-      </div><div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
 
-      </div><div className="task-card-container">
-            <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-             
-                
-            <p> Learn React</p>
-            <p> Build a React App</p>
-            <p> Deploy the App</p>
-            <p> Repeat</p>
-            <p> Profit $$$</p>
-                
-             
+                </div>
 
-      </div>
-      
-      </>
-      
+                <div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div><div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div><div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div><div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div><div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div><div className="task-card">
+                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+
+
+                    <p> Learn React</p>
+                    <p> Build a React App</p>
+                    <p> Deploy the App</p>
+                    <p> Repeat</p>
+                    <p> Profit $$$</p>
+
+
+
+                </div>
+            </div>
+
+        </>
+
     )
-  }
+}
 export default TaskCard
