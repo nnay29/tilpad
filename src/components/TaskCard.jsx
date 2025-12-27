1656ec49a08f326a84916a48fefdd5f90899f1c4
nnay29 <fodoptallayann@gmail.com>
2025-12-27 02:09:35 +0100

    feat(cards): Dynamic display of Card dates and month of week in upperpart

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,27 +2,18 @@
 import './TaskCard.css'
 import '../index.css'
 
+import { getDatesOfWeek } from '../utils/allDays'
+
 function TaskCard() {
+    const daysOfWeek = getDatesOfWeek();
 
     return (
         <>
             <div className="task-card-container">
-
-                <div className="task-card">
-                    <h2>09 Dec <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-                </div>
-
-                <div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
+                {daysOfWeek.map((day,index)=>(
+                    <div className="task-card" key={index}>
+                    <h2>{day.format('DD MMM')} <span className="day-of-week">{day.format('ddd')}</span></h2>
+                    <hr />
 
 
                     <p> Learn React</p>
@@ -30,70 +21,11 @@ function TaskCard() {
                     <p> Deploy the App</p>
                     <p> Repeat</p>
                     <p> Profit $$$</p>
+                    </div>
+                ))
 
-
-
-                </div><div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-
-                </div><div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-
-                </div><div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-
-                </div><div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-
-                </div><div className="task-card">
-                    <h2><span>09 Dec</span> <span className="day-of-week">Tue</span></h2>
-
-
-                    <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p>
-
-
-
-                </div>
+                }
+    
             </div>
 
         </>
--- src/components/UpperPart.jsx
+++ src/components/UpperPart.jsx
@@ -1,7 +1,8 @@
-import dayjs from "dayjs";
 
+import { monthOfWeek } from "../utils/allDays";
 function UpperPart(){
-    const currentMonthYear = dayjs().format('MMMM YYYY')
+    //const currentMonthYear = dayjs().format('MMMM YYYY')
+    const currentMonthYear = monthOfWeek(); //use monthOfWeek rather than direct call
     return(
         <>
         <div className="upper-part-container">
