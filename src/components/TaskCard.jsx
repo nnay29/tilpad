f816321d5b1f53a0befddb755d1eb03e2656f5af
nnay29 <fodoptallayann@gmail.com>
2026-01-03 07:26:44 +0100

    ref(All): Changed font family to Comic Sans MS  cursive. Looks good to me

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,37 +2,27 @@
 import './TaskCard.css'
 import '../index.css'
 
-//import { getDatesOfWeek } from '../utils/allDays'
-
 function TaskCard({tasks, cardDate}) {
 //    const daysOfWeek = getDatesOfWeek();
 
     return (
         <>
-            {/* <div className="task-card-container"> */}
+          
               
                     <div className="task-card">
-                    <h2>{cardDate.format('DD MMM')} <span className="day-of-week">{cardDate.format('ddd')}</span></h2>
+                    <h2> <span className="day-of-week">{cardDate.format('ddd')}</span> {cardDate.format('DD MMM')} </h2>
                     <hr />
 
                     {tasks.map((task, index) => (
                         <div className="task" key={index}>
-                            {/* <input type="checkbox" /> */}
+                            
                             <p>{task}</p>
                         </div>
                     ))}
 
-                    {/* <p> Learn React</p>
-                    <p> Build a React App</p>
-                    <p> Deploy the App</p>
-                    <p> Repeat</p>
-                    <p> Profit $$$</p> */}
+                    
                     </div>
                 
-
-    
-            {/* </div> */}
-
         </>
 
     )
