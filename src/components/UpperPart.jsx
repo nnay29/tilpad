1f45379a939bfaf3dbd75feb80045eb825448aaf
nnay29 <fodoptallayann@gmail.com>
2026-01-06 17:14:23 +0100

    feat(nav): Setting up navigation buttons into the layout

--- src/components/UpperPart.jsx
+++ src/components/UpperPart.jsx
@@ -1,13 +1,37 @@
 
 import { monthOfWeek } from "../utils/allDays";
+import { useState } from "react";
+
+import NavigationButton from "./NavigationButton.jsx";
 function UpperPart(){
-    //const currentMonthYear = dayjs().format('MMMM YYYY')
+    const [datePos, setdatePos] = useState(0);
+
+    const handleLeftClick = () => {
+         
+        setdatePos(datePos - 1);
+        // console.log(datePos);
+    }
+    const handleRightClick = () => {
+         
+        setdatePos(datePos + 1);
+        // console.log(datePos);
+    }
+
     const currentMonthYear = monthOfWeek(); //use monthOfWeek rather than direct call
     return(
         <>
+        {console.log(`current datePos: ${datePos}`)}
         <div className="upper-part-container">
             <h1>{currentMonthYear}</h1>
         </div>
+
+        <button onClick={handleLeftClick}>left</button>
+        
+        <button onClick={handleRightClick}>right</button>
+
+        <NavigationButton direction="left" />
+        <NavigationButton direction="right" />
+        
         </>
     )
 }
