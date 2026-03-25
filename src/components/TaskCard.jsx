401b16abae5fab477f907be083c62cb72b37c148
nnay29 <fodoptallayann@gmail.com>
2026-03-25 23:54:24 +0100

    docs: Adding more comments justifying the code logic

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,28 +2,22 @@
 import './TaskCard.css'
 import '../index.css'
 
-function TaskCard({tasks, cardDate,index}) {
-//    const daysOfWeek = getDatesOfWeek();
+
+function TaskCard({ tasks, cardDate, index }) {
+    //    const daysOfWeek = getDatesOfWeek();
 
     return (
-        // <>
-          
-              
-                    <div className="task-card" key={index}>
-                    <h2> <span className="day-of-week">{cardDate.format('ddd')}</span> {cardDate.format('DD MMM')} </h2>
-                    <hr />
+        <div className="task-card" key={index}>
+            <h2> <span className="day-of-week">{cardDate.format('ddd')}</span> {cardDate.format('DD MMM')} </h2>
+            <hr />
 
-                    {tasks.map((task, index) => (
-                        <div className="task" key={index}>
-                            
-                            <p>{task}</p>
-                        </div>
-                    ))}
+            {tasks.map((task, index) => (
+                <div className="task" key={index}>
+                    <p>{task}</p>
+                </div>
+            ))}
 
-                    
-                    </div>
-                
-        
+        </div>
 
     )
 }
