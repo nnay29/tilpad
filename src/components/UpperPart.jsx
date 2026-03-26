8f7507403cb17ec44340bb4b792d60cde124831a
nnay29 <fodoptallayann@gmail.com>
2026-03-26 01:49:40 +0100

    feat(navigation): Month now changes display color if we shift from Current week display.
    Might add different colors for weeks ahead or weeks backward

--- src/components/UpperPart.jsx
+++ src/components/UpperPart.jsx
@@ -20,20 +20,29 @@ function UpperPart({weekShift, setWeekShift}){ // weekShift and setWeekShift are
     }
 
     const currentMonthYear = monthOfWeek(weekShift); //use monthOfWeek rather than direct call
-    return(
+    return (
         <>
-        {console.log(`current weekShift: ${weekShift}`)}
-        <div className="upper-part-container">
-            <h1>{currentMonthYear}</h1>
-        </div>
+            {console.log(`current weekShift: ${weekShift}`)}
+            <div className="upper-part-container">
 
-        <button onClick={handleLeftClick}>left</button>
-        
-        <button onClick={handleRightClick}>right</button>
 
-        <NavigationButton direction="left" />
-        <NavigationButton direction="right" />
-        
+                {weekShift != 0 ? 
+                    <h1 style={{ color: "#03c2fc" }}>{currentMonthYear}</h1>
+
+                    :  //  Might add different colors for weeks ahead or weeks backward, so it will be three cases for conditional rendering
+                        // switch case migh be useful here
+
+                    <h1>{currentMonthYear}</h1>
+                }
+            </div>
+
+            <button onClick={handleLeftClick}>left</button>
+
+            <button onClick={handleRightClick}>right</button>
+
+            <NavigationButton direction="left" />
+            <NavigationButton direction="right" />
+
         </>
     )
 }
