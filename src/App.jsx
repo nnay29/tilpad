25ac76848612b00ccb29269671f9d31251cc7f7d
nnay29 <fodoptallayann@gmail.com>
2026-03-26 10:56:00 +0100

    feat(ui-navigation): Current day is now highlighted.
    Created a today state variable which contains the dayjs object that contains the date of today. Applied conditional rendering to highlight current day

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import './App.css'
 import TaskCardBlock from './components/TaskCardBlock'
 import { useState } from 'react'
 
+import dayjs from 'dayjs';
+
+
 function App() {
   const [weekShift, setWeekShift] = useState(0); // weekShift tracks moving forward/backward from the current week
+  const [today,setToday] = useState(dayjs()) //dayjs object that contains the date of today
 
+  //const today = dayjs();
+console.log(`today is ${today.day()}`);
   
   return (
     <>
       {/* props passed down so clickin on left-right button updates weekShift which re-renders the components with new values, hence new weeks */}
       <UpperPart weekShift={weekShift} setWeekShift={setWeekShift} />
-      <TaskCardBlock weekShift={weekShift} />
+      
+      <TaskCardBlock weekShift={weekShift} today={today} setToday = {setToday}/>
     </>
   )
 }
