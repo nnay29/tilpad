410150bc7b2630c768fbd289e189ca033f49f309
nnay29 <fodoptallayann@gmail.com>
2026-03-31 13:11:36 +0100

    chore: Setting the Non-Null Assertion Operator (!) in the create root to tell TS that the root can't be null

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( // telling TypeScript: "I know you think this might be null, but I promise you it’s not. Just trust me."
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
